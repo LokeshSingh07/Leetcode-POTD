d1d40c5388f362a90e909e70ebc446c87ee73978
Lokesh Singh <122585548+LokeshSingh07@users.noreply.github.com>
2025-02-01 12:32:13 +0530

    array

---
+++ February/1_special_array_1.cpp
@@ -0,0 +1,14 @@
+// 3151. Special Array I
+
+class Solution {
+public:
+    bool isArraySpecial(vector<int>& nums) {
+        int n = nums.size();
+
+        for(int i=0; i<n-1; i++){
+            if((nums[i]&1) == (nums[i+1]&1)) return false;
+        }
+
+        return true;
+    }
+};
