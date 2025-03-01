89b7a8bd6c76317da23adb554401efad429e584f
Lokesh Singh <122585548+LokeshSingh07@users.noreply.github.com>
2025-03-01 12:13:59 +0530

    POTD

--- February/14_product_of_the_last_K_numers.cpp
+++ February/14_product_of_the_last_K_numers.cpp
@@ -1,29 +1,29 @@
 // 1352. Product of the Last K Numbers
 
 class ProductOfNumbers {
-    public:
-        vector<int> arr;
-        ProductOfNumbers() {
-            
-        }
+public:
+    vector<int> arr;
+    ProductOfNumbers() {
         
-        void add(int num) {
-            arr.push_back(num);
-        }
-        
-        int getProduct(int k) {
-            int n = arr.size();
-            int pro = 1;
-            for(int i=n-1; i>=n-k; i--){
-                pro *= arr[i];
-            }
-            return pro;
-        }
-    };
+    }
     
-    /**
+    void add(int num) {
+        arr.push_back(num);
+    }
+    
+    int getProduct(int k) {
+        int n = arr.size();
+        int pro = 1;
+        for(int i=n-1; i>=n-k; i--){
+            pro *= arr[i];
+        }
+        return pro;
+    }
+};
+    
+/**
      * Your ProductOfNumbers object will be instantiated and called as such:
      * ProductOfNumbers* obj = new ProductOfNumbers();
      * obj->add(num);
      * int param_2 = obj->getProduct(k);
-     */
+ */
