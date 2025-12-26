e3f126753dd8d6665a22ec76d37ffd49d9637e41
Lokesh singh <lokeshsingh0089@gmail.com>
2025-12-26 13:31:14 +0530

    POTD

---
+++ December/09_Count_Operations_to_Obtain_Zero.cpp
@@ -0,0 +1,17 @@
+// 2169. Count Operations to Obtain Zero
+
+class Solution {
+public:
+    int countOperations(int num1, int num2) {
+        int result = 0;
+
+        while(num1 && num2){
+            if(num1 >= num2) num1 -= num2;
+            else num2 -= num1;
+
+            result++;
+        }
+
+        return result;
+    }
+};
